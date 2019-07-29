c303e07f74126fee50058ce5d665a0ebcb108ac0
yeremia-d <yeremia.djaja@gmail.com>
2019-07-28 20:04:20 -0400

    Finished tests for update user component.
    NOTE: DB needs to be online for application tests to pass.

--- src/test/java/com/acn/users/unitTest/UserServiceUpdateUserTest.java
+++ src/test/java/com/acn/users/unitTest/UserServiceUpdateUserTest.java
@@ -12,9 +12,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-
-import java.util.Optional;
-
+// Testing the update function for the UserService, where only the appropriate fields
+// are updated. If the incoming user object has null fields, only the fields that
+// are not null are updated.
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,7 +27,6 @@ public class UserServiceUpdateUserTest {
     private UserServiceImpl userService;
 
     private static User oldUser;
-
     private static String changedValue;
 
     @BeforeClass
@@ -47,7 +46,7 @@ public class UserServiceUpdateUserTest {
     }
 
     @Test
-    public void updateNameOnly() {
+    public void testUpdateFirstNameOnly() {
         User updated = new User();
         updated.setId(1L);
         updated.setFirstname(changedValue);
@@ -58,11 +57,140 @@ public class UserServiceUpdateUserTest {
         User result = userService.updateUser(1L, updated);
 
         // Only the first name should be updated
-        assertEquals(changedValue, result.getFirstname());
-        assertEquals(oldUser.getLastname(), result.getLastname());
-        assertEquals(oldUser.getAddress(), result.getAddress());
-        assertEquals(oldUser.getEmail(), result.getEmail());
-        assertEquals(oldUser.getPhone(), oldUser.getPhone());
+        assertEquals(changedValue,              result.getFirstname());
+        assertEquals(oldUser.getLastname(),     result.getLastname());
+        assertEquals(oldUser.getAddress(),      result.getAddress());
+        assertEquals(oldUser.getEmail(),        result.getEmail());
+        assertEquals(oldUser.getPhone(),        result.getPhone());
     }
 
+    @Test
+    public void testUpdateLastNameOnly() {
+        User updated = new User();
+        updated.setId(1L);
+        updated.setLastname(changedValue);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(oldUser.getFirstname(),    result.getFirstname());
+        assertEquals(changedValue,              result.getLastname());
+        assertEquals(oldUser.getAddress(),      result.getAddress());
+        assertEquals(oldUser.getEmail(),        result.getEmail());
+        assertEquals(oldUser.getPhone(),        result.getPhone());
+    }
+
+    @Test
+    public void testUpdateAddressOnly() {
+        User updated = new User();
+        updated.setId(1L);
+        updated.setAddress(changedValue);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(oldUser.getFirstname(),    result.getFirstname());
+        assertEquals(oldUser.getLastname(),     result.getLastname());
+        assertEquals(changedValue,              result.getAddress());
+        assertEquals(oldUser.getEmail(),        result.getEmail());
+        assertEquals(oldUser.getPhone(),        result.getPhone());
+    }
+
+    @Test
+    public void testUpdateEmailOnly(){
+        User updated = new User();
+        updated.setId(1L);
+        updated.setEmail(changedValue);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(oldUser.getFirstname(),    result.getFirstname());
+        assertEquals(oldUser.getLastname(),     result.getLastname());
+        assertEquals(oldUser.getAddress(),      result.getAddress());
+        assertEquals(changedValue,              result.getEmail());
+        assertEquals(oldUser.getPhone(),        result.getPhone());
+    }
+
+    @Test
+    public void testUpdatePhoneOnly(){
+        User updated = new User();
+        updated.setId(1L);
+        updated.setPhone(changedValue);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(oldUser.getFirstname(),    result.getFirstname());
+        assertEquals(oldUser.getLastname(),     result.getLastname());
+        assertEquals(oldUser.getAddress(),      result.getAddress());
+        assertEquals(oldUser.getEmail(),        result.getEmail());
+        assertEquals(changedValue,              result.getPhone());
+    }
+
+    @Test
+    public void testUpdateTwoFields() {
+        User updated = new User();
+        updated.setId(1L);
+        updated.setFirstname(changedValue + "firstName");
+        updated.setLastname(changedValue + "lastName");
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(changedValue + "firstName",    result.getFirstname());
+        assertEquals(changedValue + "lastName",     result.getLastname());
+        assertEquals(oldUser.getAddress(),                    result.getAddress());
+        assertEquals(oldUser.getEmail(),                      result.getEmail());
+        assertEquals(oldUser.getPhone(),                      result.getPhone());
+    }
+
+    @Test
+    public void testUpdateAllFields() {
+        User updated = new User();
+        updated.setId(1L);
+        updated.setFirstname(changedValue + "firstName");
+        updated.setLastname(changedValue + "lastName");
+        updated.setAddress(changedValue + "address");
+        updated.setEmail(changedValue + "email");
+        updated.setPhone(changedValue + "phone");
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(changedValue + "firstName",    result.getFirstname());
+        assertEquals(changedValue + "lastName",     result.getLastname());
+        assertEquals(changedValue + "address",      result.getAddress());
+        assertEquals(changedValue + "email",        result.getEmail());
+        assertEquals(changedValue + "phone",        result.getPhone());
+    }
+
+    @Test
+    public void testUpdateNoFields() {
+        User updated = new User();
+        updated.setId(1L);
+
+        Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+        Mockito.when(userRepository.getOne(1L)).thenReturn(oldUser);
+
+        User result = userService.updateUser(1L, updated);
+
+        assertEquals(oldUser.getFirstname(),    result.getFirstname());
+        assertEquals(oldUser.getLastname(),     result.getLastname());
+        assertEquals(oldUser.getAddress(),      result.getAddress());
+        assertEquals(oldUser.getEmail(),        result.getEmail());
+        assertEquals(oldUser.getPhone(),        result.getPhone());
+    }
 }
